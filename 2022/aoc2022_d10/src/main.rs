bd63724270b108411ce6764b49c29de5e7dd5226
JP <jeanpaulkhawam@protonmail.com>
2022-12-16 16:48:44 +0200

    aoc2022_d10

---
+++ 2022/aoc2022_d10/src/main.rs
@@ -0,0 +1,15 @@
+/*
+    Advent of Code 2022 / Day 10: https://adventofcode.com/2022/day/10
+    --- Day 10: Cathode-Ray Tube ---
+    Q1: Find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles.
+        What is the sum of these six signal strengths?
+    Q2: Render the image given by your program. What eight capital letters appear on your CRT?
+*/
+
+mod d10_a1;
+mod d10_a2;
+
+fn main() {
+    d10_a1::solution();
+    d10_a2::solution();
+}
